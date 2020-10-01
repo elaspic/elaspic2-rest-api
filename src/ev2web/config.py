5eb13a708d8c694c7a2a0e4af27efb8f5cde6ac5
Alexey Strokach <alex.strokach@utoronto.ca>
2020-10-01 15:36:48 -0400

    GITLAB_CI_TOKEN -> GITLAB_AUTH_TOKEN

--- src/ev2web/config.py
+++ src/ev2web/config.py
@@ -5,6 +5,6 @@ GITLAB_HOST_URL: str = os.getenv("GITLAB_HOST_URL", "https://gitlab.com")
 
 GITLAB_PROJECT_ID: int = int(os.getenv("GITLAB_PROJECT_ID", "21481523"))
 
-GITLAB_AUTH_TOKEN: Optional[str] = os.getenv("GITLAB_CI_TOKEN")
+GITLAB_AUTH_TOKEN: Optional[str] = os.getenv("GITLAB_AUTH_TOKEN")
 
 SENTRY_DSN: Optional[str] = os.getenv("SENTRY_DSN")
