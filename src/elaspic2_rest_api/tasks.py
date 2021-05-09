30422c07b23c906d5f13821ce31a7a722cb2d470
Alexey Strokach <alex.strokach@utoronto.ca>
2021-05-09 18:41:36 +0000

    Wrap monitor_tasks in an infinite while loop

--- src/elaspic2_rest_api/tasks.py
+++ src/elaspic2_rest_api/tasks.py
@@ -24,19 +24,20 @@ async def start_and_monitor_tasks():
 
 
 async def monitor_tasks(tasks):
-    for task_name, task in tasks.items():
-        if task.done():
-            error = task.exception()
-            if error is not None:
-                task.print_stack()
-                logger.error(
-                    "Task %s finished with an error: %s. Restarting...", task_name, repr(error)
-                )
-                tasks[task_name] = asyncio.create_task(task_coros[task_name](), name=task_name)
-                task_state = f"error ({type(error)}: {error})"
+    while True:
+        for task_name, task in tasks.items():
+            if task.done():
+                error = task.exception()
+                if error is not None:
+                    task.print_stack()
+                    logger.error(
+                        "Task %s finished with an error: %s. Restarting...", task_name, repr(error)
+                    )
+                    tasks[task_name] = asyncio.create_task(task_coros[task_name](), name=task_name)
+                    task_state = f"error ({type(error)}: {error})"
+                else:
+                    task_state = "done"
             else:
-                task_state = "done"
-        else:
-            task_state = "running"
-        logger.info("{:40}{:>10}".format(f"Task {task_name}:", task_state))
-    await asyncio.sleep(60)
+                task_state = "running"
+            logger.info("{:40}{:>10}".format(f"Task {task_name}:", task_state))
+        await asyncio.sleep(60)
